affbeea8045fe78c262153fa315a5ba7e7baf601
CircuitGarden <jcrossley832@gmail.com>
2026-04-12 00:17:21 +1200

    Fixed Pointer angle
    Fixed the pointer angle and added some comments to code

--- Scenes/directional_pointer.gd
+++ Scenes/directional_pointer.gd
@@ -10,14 +10,14 @@ signal direction_facing_Changed(pointer_angle)
 var pointer_rotation = 0
 
 func get_input():
-	pointer_rotation += Input.get_axis("ui_left", "ui_right") * test_rotation_Multiplier
-	if pointer_rotation > 360:
+	pointer_rotation += Input.get_axis("ui_left", "ui_right") * test_rotation_Multiplier ## changing angle depending on input with a multiplier to speed up the effect
+	if pointer_rotation > 360: ## clamping the angle to within 360 degrees since that's how the as5600 rotory encoder typically outputs
 		pointer_rotation = 0
 	elif pointer_rotation < 0:
 		pointer_rotation = 360
 	rotation_degrees = pointer_rotation
-	direction_facing_Changed.emit(rotation_degrees)
+	direction_facing_Changed.emit(rotation_degrees) ## emitting the current angle to the Player parent Node
 	#print(pointer_rotation)
 
 func _input(event: InputEvent) -> void:
-	get_input()
+	get_input() ## on input event query the get_input function (this seems to be inconsistent and laggy)
--- Scenes/player.gd
+++ Scenes/player.gd
@@ -8,25 +8,28 @@ var move_direction = Vector2()
 
 @export var speed = 400
 @export var acceleration = 4
+@export var speed_decay = 1
 
 var current_speed = 0
 
 func _on_directional_pointer_direction_facing_changed(pointer_angle: Variant) -> void:
 	player_move_direction = pointer_angle
-	print(player_move_direction)
+	#print(player_move_direction)
+	## recieving the angle from the directional pointer node
 
 
 func get_input():
 	if current_speed <= speed && current_speed >= -speed:
 		current_speed += Input.get_axis("ui_up", "ui_down") * acceleration
 	if current_speed < 0:
-		current_speed += 1
+		current_speed += speed_decay
 	elif current_speed > 0:
-		current_speed += -1
+		current_speed += -speed_decay
 	#print(current_speed)
+	## setting the current acceleration depending on how long the up or down key is held with a slow decay
 
 func _physics_process(delta):
-	move_direction = Vector2(cos(deg_to_rad(player_move_direction)) * current_speed, sin(deg_to_rad(player_move_direction)) * current_speed)
+	move_direction = Vector2(sin(deg_to_rad(-player_move_direction)) * current_speed, cos(deg_to_rad(-player_move_direction)) * current_speed) ## applying a vector to the velocity based on the angle recieved from direction_pointer
 	velocity = move_direction
 	get_input()
 	move_and_slide()
