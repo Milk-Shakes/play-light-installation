ca04455506d5dd2a04f9e83b789bebf028c45e41
CircuitGarden <jcrossley832@gmail.com>
2026-05-12 15:05:41 +1200

    Added Colour testing code to camera
    Have added some code to camera2D to test colours on the screen., currently not working yet

--- Scenes/camera.gd
+++ Scenes/camera.gd
@@ -1,15 +1,36 @@
 extends Camera2D
 
+@export_group("Display Settings")
+@export var Brightness: int = 120
+@export var MaxBrightness: int = 255
+@export var VerticalResolution: int = 512
+@export var LEDCount: Array[int]
+@export var Radius: Array[int]
+
+@onready var NumOfRings = LEDCount.size()
+@onready var ConvertedRadius: Array[int]
+@onready var RingAngle: Array[float]
+@onready var ScaleConversionFactor: int = VerticalResolution / Radius.max()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for b in range(NumOfRings):
+		ConvertedRadius[b] = Radius[b] * ScaleConversionFactor
+		RingAngle[b] = 360 / LEDCount[b]
+	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
+	for i in range(NumOfRings):
+		for c in LEDCount[i]:
+			#print(get_viewport().get_texture().get_image().get_pixel(700,400))
+			#print(c)
+			pass
+	
+	#print(get_viewport().get_texture().get_image().get_pixel(700,400))
 	#get_viewport().get_texture().get_data().get_pixel(0.1,0.1)
 	#print(get_viewport().get_texture().get_data().get_pixel(0.1,0.1))
-	pass
+	
 	
--- Scenes/directional_pointer.gd
+++ Scenes/directional_pointer.gd
@@ -9,6 +9,9 @@ signal direction_facing_Changed(pointer_angle)
 
 var pointer_rotation = 0
 
+func _input(event: InputEvent) -> void:
+	get_input() ## on input event query the get_input function (this seems to be inconsistent and laggy
+
 func get_input():
 	pointer_rotation += Input.get_axis("ui_left", "ui_right") * test_rotation_Multiplier ## changing angle depending on input with a multiplier to speed up the effect
 	if pointer_rotation > 360: ## clamping the angle to within 360 degrees since that's how the as5600 rotory encoder typically outputs
@@ -18,6 +21,3 @@ func get_input():
 	rotation_degrees = pointer_rotation
 	direction_facing_Changed.emit(rotation_degrees) ## emitting the current angle to the Player parent Node
 	#print(pointer_rotation)
-
-func _input(event: InputEvent) -> void:
-	get_input() ## on input event query the get_input function (this seems to be inconsistent and laggy)
