73e026730380b7ac26e8b2a81c7a27dceba9281a
CircuitGarden <jcrossley832@gmail.com>
2026-05-12 18:43:02 +1200

    Working colour pick system
    Got colour picking system working but it is deeply slow and laggy (sub 1 fps). Needs significant optimisation

--- Scenes/camera.gd
+++ Scenes/camera.gd
@@ -3,21 +3,36 @@ extends Camera2D
 @export_group("Display Settings")
 @export var Brightness: int = 120
 @export var MaxBrightness: int = 255
-@export var VerticalResolution: int = 512
+@export var VerticalResolution: float = 512
 @export var LEDCount: Array[int]
-@export var Radius: Array[int]
+@export var Radius: Array[float]
+
+var LEDMaxCount: int
 
 @onready var NumOfRings = LEDCount.size()
-@onready var ConvertedRadius: Array[int]
+@onready var ConvertedRadius: Array[float]
 @onready var RingAngle: Array[float]
-@onready var ScaleConversionFactor: int = VerticalResolution / Radius.max()
+@onready var ScaleConversionFactor: float = ((VerticalResolution / 2) / Radius.max())
+@onready var ViewportXCenter: int = (get_viewport_rect().size.x/2)
+@onready var ViewportYCenter: int = (get_viewport_rect().size.y/2)
+
+var LEDColourValue: Array[Color]
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Engine.max_fps = 15
+	
+	for n in range(NumOfRings):
+		LEDMaxCount = LEDMaxCount + LEDCount[n]
+	
+	ConvertedRadius.resize(NumOfRings)
+	RingAngle.resize(NumOfRings)
+	LEDColourValue.resize(LEDMaxCount)
+	
 	for b in range(NumOfRings):
 		ConvertedRadius[b] = Radius[b] * ScaleConversionFactor
-		RingAngle[b] = 360 / LEDCount[b]
+		RingAngle[b] = 360.0 / LEDCount[b]
 	#pass # Replace with function body.
 
 
@@ -25,9 +40,10 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	for i in range(NumOfRings):
 		for c in LEDCount[i]:
+			LEDColourValue[c] = (get_viewport().get_texture().get_image().get_pixel(ViewportXCenter + ConvertedRadius[i]*sin(RingAngle[i]*c),ViewportYCenter + ConvertedRadius[i]*cos(RingAngle[i]*c)))
 			#print(get_viewport().get_texture().get_image().get_pixel(700,400))
 			#print(c)
-			pass
+			#pass
 	
 	#print(get_viewport().get_texture().get_image().get_pixel(700,400))
 	#get_viewport().get_texture().get_data().get_pixel(0.1,0.1)
