90e6ea726ef63e3b918a59e861e8d9d70b9e6f79
CircuitGarden <jcrossley832@gmail.com>
2026-05-13 23:34:00 +1200

    updated camera and particles
    Camera now follows sub while particles stay put relative to world

--- Scenes/gpu_particles_2d.gd
+++ Scenes/gpu_particles_2d.gd
@@ -1,7 +1,18 @@
 extends GPUParticles2D
 
-func get_input_particles():
+@export var Player: Node
+@export var Camera: Node
+
+func _ready() -> void:
+	#$Timer.start(lifetime)    # If autostart isn't selected, will start the timer
 	pass
 
-func _input(_event: InputEvent) -> void:
-	get_input_particles() ## on input event query the get_input function (this seems to be inconsistent and laggy)
+
+func _on_timer_timeout():
+	Camera.transform = Player.transform
+	emitting = true
+	var root = get_tree().root
+	var main_scene = root.get_child(root.get_child_count() - 1)
+	get_parent().remove_child($".")
+	main_scene.add_child($".")
+	$".".set_owner(main_scene)
