991c525b5bbed461e4bc1fa87c57106553ed5fc7
CircuitGarden <jcrossley832@gmail.com>
2026-05-24 16:40:09 +1200

    Working Colour testing
    Have gotten colour testing working and correctly generating rgb values for the esp32. Next task is to get godot talking to the esp32 through serial in order to update display live

--- Scenes/camera.gd
+++ Scenes/camera.gd
@@ -14,11 +14,11 @@ var LEDMaxCount: int
 @onready var ConvertedRadius: Array[float]
 @onready var RingAngle: Array[float]
 
-@onready var ViewportHeight: float = get_viewport_rect().size.y - 1
+@onready var ViewportHeight: float = get_viewport_rect().size.y
 @onready var ViewportXCenter: int = (get_viewport_rect().size.x/2)
 @onready var ViewportYCenter: int = (ViewportHeight/2)
 
-@onready var ScaleConversionFactor: float = ((ViewportHeight / 2) / Radius.max())
+@onready var ScaleConversionFactor: float = (((ViewportHeight / 2)-10) / Radius.max())
 
 var LEDColourValue: Array[Vector3i]
 var CurrentColourValue: Color
@@ -60,15 +60,28 @@ func _DisplayColorScan(Scan):
 
 func save_to_file():
 	var file = FileAccess.open("res://save_game.dat", FileAccess.WRITE)
+	SceneTexture.save_png("Test.png")    
 	for k in LEDMaxCount:
 		file.store_string(" {" + str(LEDColourValue[k][0]) + "," + str(LEDColourValue[k][1]) + "," + str(LEDColourValue[k][2]) + "} , ")
 
-	file.close()
+	file.close() 
 
 func _unhandled_input(event):
 	if event is InputEventKey:
 		if event.pressed and event.keycode == KEY_SPACE:
 			save_to_file()
+
+#func _draw():
+#	for i in range(NumOfRings):
+#		for c in LEDCount[i]:
+#			var CurrentAngle = deg_to_rad(RingAngle[i] * c)
+#			var TestX = int(ConvertedRadius[i] * sin(CurrentAngle))
+#			var testY = int(ConvertedRadius[i] * cos(CurrentAngle))
+#			#print(RingAngle[i])
+#			#print(str(RingAngle[i]) + "  |  " + str(c) + "  |  " + str(CurrentAngle) + "  |  " + str(ConvertedRadius[i]) + "  |  " + str(ConvertedRadius[i] * sin(CurrentAngle)))
+#			draw_circle(Vector2(TestX,testY), 2, Color8(255,0,0,255), true)
+#   			#draw_circle(Vector2(0,0), 5, Color8(255,0,0,255), true)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -77,14 +90,20 @@ func _process(_delta: float) -> void:
 	var currentled = 0
 	for i in range(NumOfRings):
 		for c in LEDCount[i]:
-			CurrentColourValue = SceneTexture.get_pixel(ViewportXCenter +  ConvertedRadius[i] * sin(RingAngle[i] * c),ViewportYCenter + ConvertedRadius[i] * cos(RingAngle[i] * c))
+			var CurrentAngle = float(deg_to_rad(RingAngle[i] * c)) 
+			var TestX = int(ViewportXCenter + (ConvertedRadius[i] * sin(CurrentAngle)))
+			var testY = int(ViewportYCenter + (ConvertedRadius[i] * cos(CurrentAngle)))
+			CurrentColourValue = SceneTexture.get_pixel(TestX,testY)
+			#print(str(ViewportXCenter) + "  |  " + str(ViewportYCenter))
+			#print(str(TestX) + "  |  " + str(testY) + "  |  " + str(currentled) + "  |  " + str(CurrentAngle))
 			LEDColourValue[currentled][0] = CurrentColourValue.r8
 			LEDColourValue[currentled][1] = CurrentColourValue.g8
 			LEDColourValue[currentled][2] = CurrentColourValue.b8
 			currentled = currentled + 1
-	currentled = 0
+		#print("NEXT RING")
+	currentled = 0 
 	#_DisplayColorScan(SceneTexture)
-			
+##	queue_redraw()
 			#LEDColourValue[c] = (get_viewport().get_texture().get_image().get_pixel(ViewportXCenter + ConvertedRadius[i]*sin(RingAngle[i]*c),ViewportYCenter + ConvertedRadius[i]*cos(RingAngle[i]*c)))
 			#print(get_viewport().get_texture().get_image().get_pixel(700,400))
 			#print(c)
