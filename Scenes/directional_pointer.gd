93eaf8d503c4843dd3faddda06dca03739b29baf
CircuitGarden <jcrossley832@gmail.com>
2026-06-08 09:17:44 +1200

    Changed a shitload of stuff
    Added direction with actually speed control
    adjusted turning system
    added collision and bodged together directional damage indicators
    improved sonar a small bit

--- Scenes/directional_pointer.gd
+++ Scenes/directional_pointer.gd
@@ -3,7 +3,11 @@ extends Sprite2D
 
 var rotation_direction = 0
 
-@export var test_rotation_Multiplier = 2
+@export var Rotation_Acceleration = float(0.05)
+@export var Rotation_Decceleration = float(0.5)
+@export var Max_Rotation_Speed = float(3)
+
+var Current_Rotation_Speed = float(0)
 
 signal direction_facing_Changed(pointer_angle)
 
@@ -13,11 +17,17 @@ func _ready() -> void:
 	rotation_degrees = pointer_rotation
 	direction_facing_Changed.emit(rotation_degrees)
 
-func _input(_event: InputEvent) -> void:
-	get_input() ## on input event query the get_input function (this seems to be inconsistent and laggy
-
-func get_input():
-	pointer_rotation += Input.get_axis("ui_left", "ui_right") * test_rotation_Multiplier ## changing angle depending on input with a multiplier to speed up the effect
+func _physics_process(delta: float) -> void:
+	if (Input.get_axis("LEFT", "RIGHT") > 0 or Input.get_axis("LEFT", "RIGHT") < 0 && Current_Rotation_Speed <= Max_Rotation_Speed):
+		Current_Rotation_Speed = Current_Rotation_Speed + (Input.get_axis("LEFT", "RIGHT") * Rotation_Acceleration)
+	else: if (Current_Rotation_Speed - Rotation_Decceleration > 0):
+		Current_Rotation_Speed = Current_Rotation_Speed - Rotation_Decceleration
+	else: if (Current_Rotation_Speed + Rotation_Decceleration < 0):
+		Current_Rotation_Speed = Current_Rotation_Speed + Rotation_Decceleration
+	else:
+		Current_Rotation_Speed = 0
+		
+	pointer_rotation += Current_Rotation_Speed ## changing angle depending on input with a multiplier to speed up the effect
 	if pointer_rotation > 360: ## clamping the angle to within 360 degrees since that's how the as5600 rotory encoder typically outputs
 		pointer_rotation = 0
 	elif pointer_rotation < 0:
--- Scenes/player.gd
+++ Scenes/player.gd
@@ -6,9 +6,13 @@ var player_move_direction = 0
 
 var move_direction = Vector2()
 
-@export var speed = 400
-@export var acceleration = 4
-@export var speed_decay = 1
+@export var speed = 100
+@export var acceleration = 2
+
+@export var Submarine: Node
+@export var Collider: Node
+
+var CurrentSpeed = int(0)
 
 var current_speed = 0
 
@@ -18,18 +22,86 @@ func _on_directional_pointer_direction_facing_changed(pointer_angle: Variant) ->
 	## recieving the angle from the directional pointer node
 
 
-func get_input():
-	if current_speed <= speed && current_speed >= -speed:
-		current_speed += Input.get_axis("ui_up", "ui_down") * acceleration
-	if current_speed < 0:
-		current_speed += speed_decay
-	elif current_speed > 0:
-		current_speed += -speed_decay
-	#print(current_speed)
+
+func _on_cave_body_entered(body: Node2D) -> void:
+	if (move_direction.x > 5 && move_direction.y < 5 && move_direction.y > -5):
+		$"Submarine v2/SubmarineV2RedRear".visible = true
+	else: if (move_direction.x < -5 && move_direction.y < 5 && move_direction.y > -5):
+		$"Submarine v2/SubmarineV2RedFront".visible = true
+	else: if (move_direction.x < 5 && move_direction.x > -5 && move_direction.y > 5):
+		$"Submarine v2/SubmarineV2RedBottom".visible = true
+	else: if (move_direction.x < 5 && move_direction.x > -5 && move_direction.y < -5):
+		$"Submarine v2/SubmarineV2RedTop".visible = true
+		
+	else: if (move_direction.x > 5 && move_direction.y > 5):
+		$"Submarine v2/SubmarineV2RedRear".visible = true
+		$"Submarine v2/SubmarineV2RedBottom".visible = true
+	else: if (move_direction.x < -5 && move_direction.y < -5):
+		$"Submarine v2/SubmarineV2RedFront".visible = true
+		$"Submarine v2/SubmarineV2RedTop".visible = true
+	else: if (move_direction.x < -5 && move_direction.y > 5):
+		$"Submarine v2/SubmarineV2RedBottom".visible = true
+		$"Submarine v2/SubmarineV2RedFront".visible = true
+	else: if (move_direction.x > 5 && move_direction.y < -5):
+		$"Submarine v2/SubmarineV2RedTop".visible = true
+		$"Submarine v2/SubmarineV2RedRear".visible = true
+	#Submarine.visible = false
+	current_speed = -speed
+	CurrentSpeed = 0
+	$DirectionalPointer/LowSpeed.visible = false
+	$DirectionalPointer/MediumSpeed.visible = false
+	$DirectionalPointer/HighSpeed.visible = false
+
+
+func _input(event):
+	if (Input.get_axis("DOWN", "UP") > 0) && !event.is_echo() && event.is_pressed():
+		if (CurrentSpeed < 3):
+			CurrentSpeed = CurrentSpeed + 1
+	else: if (Input.get_axis("DOWN", "UP") < 0) && !event.is_echo() && event.is_pressed():
+		if (CurrentSpeed > 0):
+			CurrentSpeed = CurrentSpeed - 1
+
+	if (CurrentSpeed == 0):
+		$DirectionalPointer/LowSpeed.visible = false
+		$DirectionalPointer/MediumSpeed.visible = false
+		$DirectionalPointer/HighSpeed.visible = false
+	else: if (CurrentSpeed == 1):
+		$DirectionalPointer/LowSpeed.visible = true
+		$DirectionalPointer/MediumSpeed.visible = false
+		$DirectionalPointer/HighSpeed.visible = false
+	else: if (CurrentSpeed == 2):
+		$DirectionalPointer/LowSpeed.visible = true
+		$DirectionalPointer/MediumSpeed.visible = true
+		$DirectionalPointer/HighSpeed.visible = false
+	else: if (CurrentSpeed == 3):
+		$DirectionalPointer/LowSpeed.visible = true
+		$DirectionalPointer/MediumSpeed.visible = true
+		$DirectionalPointer/HighSpeed.visible = true
+
+	#current_speed = (CurrentSpeed * (speed/3))
+	print(current_speed)
 	## setting the current acceleration depending on how long the up or down key is held with a slow decay
 
-func _physics_process(_delta):
-	move_direction = Vector2(sin(deg_to_rad(-player_move_direction)) * current_speed, cos(deg_to_rad(-player_move_direction)) * current_speed) ## applying a vector to the velocity based on the angle recieved from direction_pointer
+func _physics_process(delta):
+	
+	#if ($CollisionShape2D.)
+	if (current_speed > 9):
+		$"Submarine v2/SubmarineV2RedTop".visible = false
+		$"Submarine v2/SubmarineV2RedRear".visible = false
+		$"Submarine v2/SubmarineV2RedBottom".visible = false
+		$"Submarine v2/SubmarineV2RedFront".visible = false
+	
+	if (current_speed < ((CurrentSpeed * (speed/3))-acceleration)):
+		current_speed = current_speed + acceleration
+	else: if (current_speed > ((CurrentSpeed * (speed/3))+acceleration)):
+		current_speed = current_speed - acceleration
+	else:
+		current_speed = CurrentSpeed * (speed/3)
+	move_direction = -(Vector2(sin(deg_to_rad(-player_move_direction)) * current_speed, cos(deg_to_rad(-player_move_direction)) * current_speed)) ## applying a vector to the velocity based on the angle recieved from direction_pointer
 	velocity = move_direction
-	get_input()
+	#get_input()
 	move_and_slide()
+	if (move_and_slide() == true):
+		Submarine.visible = false
+		current_speed = 0
+		CurrentSpeed = 0
